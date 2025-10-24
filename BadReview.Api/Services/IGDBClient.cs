a827b20cc21e44fcc203d5779a32d08eff7d00c4
Manuel <manuelmhs114@gmail.com>
2025-10-23 21:16:22 -0300

    endpoints

--- BadReview.Api/Services/IGDBClient.cs
+++ BadReview.Api/Services/IGDBClient.cs
@@ -3,63 +3,64 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http.Headers;
 
-namespace BadReview.Api.Services
-{
+namespace BadReview.Api.Services;
 
-    public record  IGDBQueryOptions
+public record IGDBQueryOptions
+{
+    public int? Id { get; init; } = null;
+    public int Limit { get; init; } = 10;
+    public string[] Fields { get; init; } = [];
+}
+
+public class IGDBClient
+{
+    private readonly HttpClient _httpClient;
+    private string? _clientId;
+    private string? _bearerToken;
+
+    //private static string? _clientId;
+    //private static string? _bearerToken;
+
+    public IGDBClient(HttpClient client, IConfiguration config)
     {
-        public int? Id { get; init; } = null;
-        public int Limit { get; init; } = 10;
-        public string[] Fields { get; init; } = [];
+        _httpClient = client;
+
+        this._clientId = config["IGDB:ClientId"];
+        this._bearerToken = config["IGDB:AccessToken"];
+
+        if (string.IsNullOrEmpty(_clientId) || string.IsNullOrEmpty(_bearerToken))
+            throw new ArgumentNullException();
     }
 
-    public class IGDBClient
+    /*private static async Task GetCredentials()
     {
-        private readonly HttpClient _httpClient;
-        private string? _clientId;
-        private string? _bearerToken;
+        _clientId = "k0z2r0lhjxj77ztbr5z45cg9iba8b2";
+        _bearerToken = "6ilx1kgnyqnvc4vsfl9lsr3yq9b7n0";
+    }*/
 
-        //private static string? _clientId;
-        //private static string? _bearerToken;
-
-        public IGDBClient(HttpClient client, IConfiguration config)
-        {
-            _httpClient = client;
-
-            this._clientId = config["IGDB:ClientId"];
-            this._bearerToken = config["IGDB:AccessToken"];
-
-            if (string.IsNullOrEmpty(_clientId) || string.IsNullOrEmpty(_bearerToken))
-                throw new ArgumentNullException();
-        }
-
-        /*private static async Task GetCredentials()
-        {
-            _clientId = "k0z2r0lhjxj77ztbr5z45cg9iba8b2";
-            _bearerToken = "6ilx1kgnyqnvc4vsfl9lsr3yq9b7n0";
+    // devolver json formateado con todos los campos incluidos en fields, mapeados a la clase Game
+    public async Task<HttpResponseMessage> GetGamesAsync(IGDBQueryOptions options)
+    {
+        /*if (_clientId is null || _bearerToken is null) {
+            await GetCredentials();
         }*/
 
-        public async Task<HttpResponseMessage> GetGamesAsync(IGDBQueryOptions options)
-        {
-            /*if (_clientId is null || _bearerToken is null) {
-                await GetCredentials();
-            }*/
+        _httpClient.DefaultRequestHeaders.Add("Client-ID", _clientId);
+        _httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {_bearerToken}");
+        _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-            _httpClient.DefaultRequestHeaders.Add("Client-ID", _clientId);
-            _httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {_bearerToken}");
-            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        string bodyString =
+            options.Id is null
+                ? $"fields {string.Join(", ", options.Fields)}; limit {options.Limit};"
+                : $"fields {string.Join(", ", options.Fields)}; where id = {options.Id};";
+        // Ejemplo de cuerpo de consulta IGDB
+        var body = new StringContent(bodyString, Encoding.UTF8, "text/plain");
+        Console.WriteLine(bodyString);
 
-            string bodyString =
-                options.Id is null
-                    ? $"fields {string.Join(", ", options.Fields)}; limit {options.Limit};"
-                    : $"fields {string.Join(", ", options.Fields)}; where id = {options.Id};";
-            // Ejemplo de cuerpo de consulta IGDB
-            var body = new StringContent(bodyString, Encoding.UTF8, "text/plain");
-            Console.WriteLine(bodyString);
+        var response = await _httpClient.PostAsync("games", body);
 
-            var response = await _httpClient.PostAsync("games", body);
+        Console.WriteLine(await response.Content.ReadAsStringAsync());
 
-            return response;
-        }
+        return response;
     }
 }
