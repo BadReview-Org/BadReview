8e53407d64e04b9196ccbffd9429b88f5dcf7c9c
Manuel <manuelmhs114@gmail.com>
2025-11-13 19:43:05 -0300

    form fluent validation

--- BadReview.Client/Utils/FormClasses.cs
+++ BadReview.Client/Utils/FormClasses.cs
@@ -15,8 +15,8 @@ public class RegisterForm
     public string Username { get; set; } = null!;
     public string Password { get; set; } = null!;
     public string RepeatPassword { get; set; } = null!;
+    public string Email { get; set; } = null!;
     public string? FullName { get; set; } = null;
-    public string? Email { get; set; } = null;
     public DateTime? Birthday { get; set; } = null;
     public int? Country { get; set; } = null;
 }
@@ -26,10 +26,10 @@ public class LoginFormValidator : AbstractValidator<LoginForm>
     public LoginFormValidator()
     {
         // Username
-        RuleFor(x => x.Username).UsernameRule();
+        RuleFor(x => x.Username).Cascade(CascadeMode.Stop).UsernameRule();
 
         // Password
-        RuleFor(x => x.Password).PasswordRule();
+        RuleFor(x => x.Password).Cascade(CascadeMode.Stop).PasswordRule();
     }
 }
 
@@ -38,18 +38,18 @@ public class RegisterFormValidator : AbstractValidator<RegisterForm>
     public RegisterFormValidator()
     {
         // Username
-        RuleFor(x => x.Username).UsernameRule();
+        RuleFor(x => x.Username).Cascade(CascadeMode.Stop).UsernameRule();
 
         // Password
-        RuleFor(x => x.Password).PasswordRule();
+        RuleFor(x => x.Password).Cascade(CascadeMode.Stop).PasswordRule();
         RuleFor(x => x.RepeatPassword).PasswordRule();
 
-        RuleFor(x => x)
+        /*RuleFor(x => x)
             .Must(x => x.Password is null || x.RepeatPassword is null ||
-                  x.Password == x.RepeatPassword);
+                  x.Password == x.RepeatPassword);*/
 
         // Email
-        RuleFor(x => x.Email)
+        RuleFor(x => x.Email).Cascade(CascadeMode.Stop)
             .NotEmpty().WithMessage("Email is required.")
             .MaximumLength(30)
             .Matches("^[a-zA-Z0-9@._-]+$")
@@ -62,8 +62,8 @@ public class RegisterFormValidator : AbstractValidator<RegisterForm>
         // FullName (solo letras y espacios, 40 caracteres máx)
         RuleFor(x => x.FullName)
             .MaximumLength(40)
+                .WithMessage("Full name is too long: must contain at most 40 characters.")
             .Matches(@"^[a-zA-Z ]*$")
-                .When(x => !string.IsNullOrEmpty(x.FullName))
                 .WithMessage("Full name must only contain letters and whitespaces.");
 
         // Birthday (mínimo 12 años)
