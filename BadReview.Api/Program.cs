56ca362bb242f410c96c38c967df3ebaf04cd478
Manuel <manuelmhs114@gmail.com>
2025-12-04 17:33:18 -0300

    light palette fix

--- BadReview.Api/Program.cs
+++ BadReview.Api/Program.cs
@@ -1,10 +1,19 @@
 using BadReview.Api.Endpoints;
 using BadReview.Api.Configuration;
+using Microsoft.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "BadReview API",
+        Description = "An API for managing game reviews and user profiles."
+    });
+});
 
 builder
     .AddCorsConfig()
